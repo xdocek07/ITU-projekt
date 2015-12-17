cc26f1fa49219530f74deda14f1ceca9ad4b8be2
Tomáš <tomas.susovsky@gmail.com>
2015-12-17 21:02:59 +0100

    fix merge

--- model.cpp
+++ model.cpp
@@ -9,13 +9,7 @@ Model::Model()
       edges{}
 {
     previous = nullptr;
-<<<<<<< HEAD
-=======
     itemId = 0;
-
-
-
->>>>>>> 912a5a30c8ea52f3491e17d21039fa5e3c18bca7
 }
 
 Model::~Model()
@@ -34,18 +28,6 @@ Model::~Model()
 void Model::loadTest()
 {
     Node *n1, *n2, *n3, *n4;
-<<<<<<< HEAD
-    int i;
-    n1 = new Node(i = ++itemId, "fuckit", Qt::green, 50, 50, 30);
-    nodes[i] = n1;
-    n2 = new Node(i = ++itemId, "ITU", Qt::red, 90, 90, 40);
-    nodes[i] = n2;
-    n3 = new Node(i = ++itemId, "flow");
-    nodes[i] = n3;
-    n4 = new Node(i = ++itemId, "ITU2", Qt::yellow, 95, 95, 80);
-    nodes[i] = n4;
-=======
-
     n1 =  addNode("fuckit", Qt::green, 50, 50, 30);
 
     n2 = addNode("ITU", Qt::red, 90, 90, 40);
@@ -53,13 +35,9 @@ void Model::loadTest()
     n3 = addNode("flow");
 
     n4 = addNode("ITU2", Qt::yellow, 95, 95, 80);
->>>>>>> 912a5a30c8ea52f3491e17d21039fa5e3c18bca7
-
     addEdge(n1, n2, "fuckinflow");
 }
 
-<<<<<<< HEAD
-=======
 Node *Model::addNode(const QString label, const QColor color, int x, int y, int len)
 {
     unsigned int id = ++itemId;
@@ -82,19 +60,17 @@ Node *Model::addNode(Node *node)
     return nodes[id];
 }
 
->>>>>>> 912a5a30c8ea52f3491e17d21039fa5e3c18bca7
-void Model::addEdge(Node *from, Node *to, QString label)
+Edge *Model::addEdge(Node *from, Node *to, QString label)
 {
     int i = ++itemId;
     edges[i] = new Edge(i, from, to, label);
     from->addEdge(edges[i]);
     to->addEdge(edges[i]);
-<<<<<<< HEAD
-=======
+
+    return edges[i];
 }
 
 void Model::saveAction(QUndoCommand *newCommand)
 {
     m_undoStack->push(newCommand);
->>>>>>> 912a5a30c8ea52f3491e17d21039fa5e3c18bca7
 }
--- model.h
+++ model.h
@@ -26,28 +26,22 @@ public:
     std::map<int, Node *> &getNodes() {return nodes;}
     std::map<int, Edge *> &getEdges() {return edges;}
 
-<<<<<<< HEAD
-    void addEdge(Node *from, Node *to, QString label = "");
-=======
+    Edge *addEdge(Node *from, Node *to, QString label = "");
     unsigned int getAutoincrement() {return ++itemId;}
 
     Node *addNode(const QString label = "", const QColor color = Qt::gray, int x = 0, int y = 0, int len = 20);
     Node *addNode(Node *node);
-    void addEdge(Node *from, Node *to, QString label = "");
     void saveAction(QUndoCommand *newCommand);
 
-    QUndoStack *undoStack() const {return m_undoStack;}
->>>>>>> 912a5a30c8ea52f3491e17d21039fa5e3c18bca7
+    QUndoStack *undoStack() const {return m_undoStack;};
 
 private:    
     std::map<int, Node *> nodes;
     std::map<int, Edge *> edges;
     unsigned int itemId;
     Node *previous;
-<<<<<<< HEAD
-=======
     QUndoStack *m_undoStack;
->>>>>>> 912a5a30c8ea52f3491e17d21039fa5e3c18bca7
+
 };
 
 #endif // MODEL_H
